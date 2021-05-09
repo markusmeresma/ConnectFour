ef662b65ea1fddbfc375ec3662087595a44d6da0
markusmeresma <markus.meresma@gmail.com>
2021-05-09 13:15:07 +0300

    Row and col check working

--- connect4.c
+++ connect4.c
@@ -3,10 +3,11 @@
 
 #define ROWS 6
 #define COLS 7
+#define FOUR 4
 char game_board[ROWS][COLS];
 char header[] = "  1   2   3   4   5   6   7";
 char row[] = "+---+---+---+---+---+---+---+";
-int game_status = 1; // set game status 0 in check methods if somebody won
+int game_status = 1;
 
 void print_board();
 void init_game_board();
@@ -33,8 +34,26 @@ int main(void)
     {
         add_disc(player_A);
         print_board();
+        
+        // Check if A is a winner
+        if(check_board(player_A))
+        {
+            printf("Player X won!\n");
+            game_status = 0;
+            break;
+        }
+
         add_disc(player_B);
         print_board();
+
+        // Check if B is a winner
+        if(check_board(player_B))
+        {
+            printf("Player O won!\n");
+            game_status = 0;
+            break;
+        }
+        
     }
     
 
@@ -99,37 +118,51 @@ void insert(int pos, char disc)
 void add_disc(char player)
 {
     int move;
+    int input_flag = 1;
 
-    printf("Enter your move player %c: \n", player);
-    scanf("%d", &move);
-
-    switch (move)
+    do
     {
-        case 1:
-            insert(0, player);
-            break;
-        case 2:
-            insert(1, player);
-            break;
-        case 3:
-            insert(2, player);
-            break;
-        case 4:
-            insert(3, player);
-            break;
-        case 5:
-            insert(4, player);
-            break;
-        case 6:
-            insert(5, player);
-            break;
-        case 7:
-            insert(6, player);
-            break;
-            
-        default:
-            printf("Invalid input!\n");
+        printf("Enter your move player %c: \n", player);
+        scanf("%d", &move);
+
+        switch (move)
+        {
+            case 1:
+                insert(0, player);
+                input_flag = 0;
+                break;
+            case 2:
+                insert(1, player);
+                input_flag = 0;
+                break;
+            case 3:
+                insert(2, player);
+                input_flag = 0;
+                break;
+            case 4:
+                insert(3, player);
+                input_flag = 0;
+                break;
+            case 5:
+                insert(4, player);
+                input_flag = 0;
+                break;
+            case 6:
+                insert(5, player);
+                input_flag = 0;
+                break;
+            case 7:
+                insert(6, player);
+                input_flag = 0;
+                break;
+                
+            default:
+                printf("Invalid input!\n");
         }
+
+    } while (input_flag);
+    
+    
 }
 
 // Method to print welcome message
@@ -140,8 +173,46 @@ void print_welcome_message()
     printf(" *******************\n\n");
 }
 
+// Method to check the state of the game
 int check_board(char player)
 {
-    // Add checking functionality here
+    int i, j;
+    int count;
+
+    // Check 4 in a row
+    for(i = 0; i < ROWS; i++)
+    {
+        count = 0;
+        for(j = 0; j < COLS; j++)
+        {
+            if(game_board[i][j] == player)
+            {
+                count++;
+                if(count == FOUR)
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    // Check 4 in a column
+    for(i = 0; i < COLS; i++)
+    {
+        count = 0;
+        for(j = 0; j < ROWS; j++)
+        {
+            if(game_board[j][i] == player)
+            {
+                count++;
+                if(count == FOUR)
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+
+
     return 0;
 }
